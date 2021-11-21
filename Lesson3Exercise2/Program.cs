0d65782d83fa4f13809af4f9e4aa6644e7c25e81
Evgenia Chatzi <e.chatzi@kaizengaming.com>
2021-11-21 15:43:56 +0200

    adds the second exercise of the lesson 3

---
+++ Lesson3Exercise2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lesson3Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int loginAttempts = 0;
+                string correctUsername = "Evgenia";
+                string correctPassword = "Pass123";
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Enter username");
+                    string username = Console.ReadLine();
+                    Console.WriteLine("Enter password");
+                    string password = Console.ReadLine();
+
+                    if (username != correctUsername || password != correctPassword)
+                        loginAttempts++;
+                    else
+                        break;
+                }
+
+                if (loginAttempts > 5)
+                    Console.WriteLine("You failed to login!");
+                else
+                    Console.WriteLine("Login successful!");
+            }
+        }
+    }
+}
