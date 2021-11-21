17e20daf32eadc2fa8df8238641feba2c2fc3241
Evgenia Chatzi <e.chatzi@kaizengaming.com>
2021-11-21 16:56:15 +0200

    adds the exercise 4 of the lesson3

---
+++ Lesson3Exercise4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lesson3Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yaer;
+            Console.WriteLine("Input a year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (year % 400 == 0)
+            {
+                Console.WriteLine("The year is leap");
+            }
+            else if (year % 100 == 0)
+            {
+                Console.WriteLine("The year is not leap");
+            }
+            else if (year % 400 == 0)
+            {
+                Console.WriteLine("The year is leap");
+            }
+            else
+            {
+                Console.WriteLine("The year is not leap");
+            }
+        }
+    }
+}
