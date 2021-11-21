21dcccaf2f4c5575b0eae168606d09ecf5e21263
Evgenia Chatzi <e.chatzi@kaizengaming.com>
2021-11-21 19:06:25 +0200

    adds the fifth exercise of the 3rd lesson

---
+++ Lesson3Exercise5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lesson3Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string symbol = "*";
+            const int counter = 10;
+
+            for (int k = counter; k > 0; k--)
+            {
+                for(int j = 0; j < k; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+
+
+            for (int r = 0; r < counter; r++)
+            {
+                for(int c = 0; c <= r; c++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+    }
+}
