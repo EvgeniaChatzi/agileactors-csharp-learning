bc3d1f2d73e2a25056443fe8b9b65da0736b9d8e
Evgenia Chatzi <e.chatzi@kaizengaming.com>
2021-11-21 16:15:08 +0200

    adds the third exercise of thge lesson3

---
+++ Lesson3Exercise3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson3Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myNumber;
+
+            Console.WriteLine("Enter a number: ");
+            myNumber = Convert.ToSingle(Console.ReadLine());
+
+            if (myNumber % 2 == 0)
+            {
+                Console.WriteLine("the number is even");
+            }
+            else
+            {
+                Console.WriteLine("the number is not even");
+            }
+        }
+    }
+}
